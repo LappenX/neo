6d8768768fb5651fedc178f8cfb2b604df9ec25a
LappenX <lappenx@users.noreply.github.com>
2017-09-27 20:52:23 +0200

    Add RowMajorIndexStrategy

--- packages/neo/src/tensor/TensorIndexStrategy.h
+++ packages/neo/src/tensor/TensorIndexStrategy.h
@@ -14,6 +14,17 @@ struct ColMajorIndexStrategy
   static size_t toIndex(const Vector<TVectorType, TElementType, TRank>& dims, TCoords&&... coords);
 };
 
+struct RowMajorIndexStrategy
+{
+  template <size_t... TDims, typename... TCoords, ENABLE_IF_ARE_SIZE_T(TCoords...)>
+  __host__ __device__
+  static size_t toIndex(TCoords&&... coords);
+
+  template <typename TVectorType, typename TElementType, size_t TRank, typename... TCoords>
+  __host__ __device__
+  static size_t toIndex(const Vector<TVectorType, TElementType, TRank>& dims, TCoords&&... coords);
+};
+
 #include "TensorIndexStrategy.hpp"
 
 } // end of ns tensor
--- packages/neo/src/tensor/TensorIndexStrategy.hpp
+++ packages/neo/src/tensor/TensorIndexStrategy.hpp
@@ -48,6 +48,13 @@ struct ColMajorIndexStrategyHelper2
   {
     return coord0;
   }
+
+  template <size_t I = 0, typename TVectorType, typename TElementType, size_t TRank>
+  __host__ __device__
+  static size_t toIndex(const Vector<TVectorType, TElementType, TRank>& dims)
+  {
+    return 0;
+  }
 };
 
 template <>
@@ -86,4 +93,175 @@ size_t ColMajorIndexStrategy::toIndex(const Vector<TVectorType, TElementType, TR
 {
   static_assert(TRank != DYN, "Must be static vector!");
   return detail::ColMajorIndexStrategyHelper2<math::lt(0, TRank)>::toIndex(dims, util::forward<TCoords>(coords)...);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+namespace detail {
+
+struct RowMajorIndexStrategyHelper1
+{
+  template <size_t TDim0, size_t TDim1, size_t... TDims, typename... TRestCoords>
+  __host__ __device__
+  static size_t toIndex(size_t coord0, size_t coord1, TRestCoords&&... coords)
+  {
+    return toIndex<TDim0 * TDim1, TDims...>(coord0 * TDim1 + coord1, util::forward<TRestCoords>(coords)...);
+  }
+
+  template <size_t TDim0, size_t TDim1, size_t... TDims>
+  __host__ __device__
+  static size_t toIndex(size_t coord0)
+  {
+    return toIndex<TDim0 * TDim1, TDims...>(coord0 * TDim1);
+  }
+
+  template <size_t TDim0, size_t TDim1, size_t... TDims>
+  __host__ __device__
+  static size_t toIndex()
+  {
+    return 0;
+  }
+
+
+
+  template <size_t TDim0, typename... TRestCoords>
+  __host__ __device__
+  static size_t toIndex(size_t coord0, size_t coord1, TRestCoords&&... coords)
+  {
+    ASSERT(coord1 == 0, "Coordinate out of bounds");
+    return toIndex<TDim0>(coord0, util::forward<TRestCoords>(coords)...);
+  }
+
+  template <size_t TDim0>
+  __host__ __device__
+  static size_t toIndex(size_t coord0)
+  {
+    return coord0;
+  }
+
+
+
+  template <typename... TRestCoords>
+  __host__ __device__
+  static size_t toIndex(size_t coord0, size_t coord1, TRestCoords&&... coords)
+  {
+    ASSERT(coord0 == 0, "Coordinate out of bounds");
+    return toIndex(coord1, util::forward<TRestCoords>(coords)...);
+  }
+
+  __host__ __device__
+  static size_t toIndex(size_t coord0)
+  {
+    ASSERT(coord0 == 0, "Coordinate out of bounds");
+    return 0;
+  }
+};
+
+template <bool TInDimRange, bool TInDimRangePlusOne>
+struct RowMajorIndexStrategyHelper2
+{
+  template <size_t I = 0, typename TVectorType, typename TElementType, size_t TRank, typename... TRestCoords>
+  __host__ __device__
+  static size_t toIndex(const Vector<TVectorType, TElementType, TRank>& dims, size_t coord0, size_t coord1, TRestCoords&&... coords)
+  {
+    return RowMajorIndexStrategyHelper2<math::lt(I + 1, TRank), math::lt(I + 2, TRank)>::template toIndex<I + 1>
+      (dims, coord0 * dims(I + 1) + coord1, util::forward<TRestCoords>(coords)...);
+  }
+
+  template <size_t I = 0, typename TVectorType, typename TElementType, size_t TRank>
+  __host__ __device__
+  static size_t toIndex(const Vector<TVectorType, TElementType, TRank>& dims, size_t coord0)
+  {
+    return coord0 * dims(I + 1);
+  }
+
+  template <size_t I = 0, typename TVectorType, typename TElementType, size_t TRank>
+  __host__ __device__
+  static size_t toIndex(const Vector<TVectorType, TElementType, TRank>& dims)
+  {
+    return 0;
+  }
+};
+
+template <>
+struct RowMajorIndexStrategyHelper2<true, false>
+{
+  template <size_t I = 0, typename TVectorType, typename TElementType, size_t TRank, typename... TRestCoords>
+  __host__ __device__
+  static size_t toIndex(const Vector<TVectorType, TElementType, TRank>& dims, size_t coord0, size_t coord1, TRestCoords&&... coords)
+  {
+    ASSERT(coord1 == 0, "Coordinate out of bounds");
+    return toIndex<I>(dims, coord0, util::forward<TRestCoords>(coords)...);
+  }
+
+  template <size_t I = 0, typename TVectorType, typename TElementType, size_t TRank>
+  __host__ __device__
+  static size_t toIndex(const Vector<TVectorType, TElementType, TRank>& dims, size_t coord0)
+  {
+    return coord0;
+  }
+
+  template <size_t I = 0, typename TVectorType, typename TElementType, size_t TRank>
+  __host__ __device__
+  static size_t toIndex(const Vector<TVectorType, TElementType, TRank>& dims)
+  {
+    return 0;
+  }
+};
+
+template <>
+struct RowMajorIndexStrategyHelper2<false, false>
+{
+  template <size_t I = 0, typename TVectorType, typename TElementType, size_t TRank, typename... TRestCoords>
+  __host__ __device__
+  static size_t toIndex(const Vector<TVectorType, TElementType, TRank>& dims, size_t coord0, size_t coord1, TRestCoords&&... coords)
+  {
+    ASSERT(coord1 == 0, "Coordinate out of bounds");
+    return toIndex<I>(dims, coord1, util::forward<TRestCoords>(coords)...);
+  }
+
+  template <size_t I = 0, typename TVectorType, typename TElementType, size_t TRank>
+  __host__ __device__
+  static size_t toIndex(const Vector<TVectorType, TElementType, TRank>& dims, size_t coord0)
+  {
+    ASSERT(coord0 == 0, "Coordinate out of bounds");
+    return 0;
+  }
+
+  template <size_t I = 0, typename TVectorType, typename TElementType, size_t TRank>
+  __host__ __device__
+  static size_t toIndex(const Vector<TVectorType, TElementType, TRank>& dims)
+  {
+    return 0;
+  }
+};
+
+} // end of ns detail
+
+template <size_t... TDims, typename... TCoords, typename>
+__host__ __device__
+size_t RowMajorIndexStrategy::toIndex(TCoords&&... coords)
+{
+  return detail::RowMajorIndexStrategyHelper1::toIndex<TDims...>(util::forward<TCoords>(coords)...);
+}
+
+template <typename TVectorType, typename TElementType, size_t TRank, typename... TCoords>
+__host__ __device__
+size_t RowMajorIndexStrategy::toIndex(const Vector<TVectorType, TElementType, TRank>& dims, TCoords&&... coords)
+{
+  static_assert(TRank != DYN, "Must be static vector!");
+  return detail::RowMajorIndexStrategyHelper2<math::lt(0, TRank), math::lt(1, TRank)>::toIndex(dims, util::forward<TCoords>(coords)...);
 }
